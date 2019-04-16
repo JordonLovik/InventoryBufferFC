be8d2dfae28c1e4facd01c650687ae035637f2df
Jordon Lovik <JordonLovik@gmail.com>
2019-04-15 21:38:46 -0700

    Update uploadftp.py

--- data/uploadftp.py
+++ data/uploadftp.py
@@ -1,7 +1,7 @@
 import ftplib
 
-ftp = ftplib.FTP('10.1.1.5')
-ftp.login('hubsoft', 'Mgfj9RZRaz')
+ftp = ftplib.FTP('*.*.*.*')
+ftp.login('********', '*********')
 ftp.cwd('s/')
 ftp.storbinary("STOR X01_Inventory.txt", open("X01_Inventory.txt", "rb"))
 #ftp.storbinary('STOR ' + 'X01_Inventory.txt',open('X01_Inventory.txt', 'wb'))
